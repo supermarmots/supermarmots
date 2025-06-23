c1b247b66f4c3a3e4e9a5a2838a8c4687d67d410
ChanWoo Yun <yunchanwo@naver.com>
2025-06-23 12:52:16 +0900

    Feat: 오전 실습 ( js 실습 )

---
+++ study/0623/main.js
@@ -0,0 +1,19 @@
+// alert("Hello World!");
+// console.log("Hello World!");
+// document.write("<h1>Document Write</h1>");
+    
+// let name = prompt("당신의 이름은?");
+// alert("안녕하세요, " + name + "님!");2001
+
+// let currentYear = 2025;
+// let birthYear = prompt("태어난 해를 입력하세요");
+// let age = currentYear - birthYear + 1 ;
+// alert("당신의 나이는 " + age + "세입니다.");
+
+let name = "철수";
+let age = 18;
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
