1ce1b210ab77cfc2407bb998e1cf9e60b9c40ccd
ChanWoo Yun <yunchanwo@naver.com>
2025-06-25 10:13:46 +0900

    Update: js 중급문제 완료

--- study/JavaScript Intermediate/Intermediate.js
+++ study/JavaScript Intermediate/Intermediate.js
@@ -53,3 +53,45 @@ function sol6() {
     const count = str.toLowerCase().split('').filter(char => vowels.includes(char)).length;
     console.log("모음 개수:", count);
 }
+
+function sol7() {
+    for (let i = 2; i<10; i++) {
+        console.log(`${i}단`);
+        for ( let j = 1; j < 10; j++) {
+             console.log(`${i} * ${j} = ${i * j}\n`);
+        }
+    }
+}
+
+function sol8() {
+    let arr = [1, 2, 3, 4, 5]
+    let sum = 0;
+    for(let i=0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log("배열의 합: "+ sum );
+}
+
+function sol9() {
+    const hello = "hello";
+    rev = hello.split("").reverse().join("");
+    console.log(rev);   
+}
+
+function sol10() {
+    for (let i = 2; i <= 100; i++) {
+        if (isPrime(i)) {
+            console.log(i);
+        }
+    }
+}
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
