118df663b65ede4968e86a7a8467a922a6a16a88
ChanWoo Yun <yunchanwo@naver.com>
2025-07-01 16:13:23 +0900

    타입스크립트 기본 자료형 연습

--- study/0701/hello.js
+++ study/0701/hello.js
@@ -1,2 +1,23 @@
-"use strict";
 console.log("Hi World!");
+function greet(name) {
+    return "Hello, ".concat(name);
+}
+console.log(greet("marmot"));
+var n = 1;
+var b = true;
+var s = "Hello";
+var o = {};
+// 추론
+var n1 = 1;
+var b1 = true;
+var s1 = "Hello";
+var o1 = {};
+var a = 0;
+a = "Hello";
+a = true;
+a = {};
+var u = undefined;
+// u = 1;
+var count = 10, message = "Your count";
+var result = "".concat(message, " is ").concat(count);
+console.log(result);
--- study/0701/hello.ts
+++ study/0701/hello.ts
@@ -1,1 +1,31 @@
 console.log("Hi World!");
+
+function greet(name: string): string {
+  return `Hello, ${name}`;
+}
+
+console.log(greet("marmot"));
+
+let n: number = 1;
+let b: boolean = true;
+let s: string = "Hello";
+let o: object = {};
+
+// 추론
+let n1 = 1;
+let b1 = true;
+let s1 = "Hello";
+let o1 = {};
+
+let a: any = 0;
+a = "Hello";
+a = true;
+a = {};
+
+let u: undefined = undefined;
+// u = 1;
+
+let count = 10,
+  message = "Your count";
+let result = `${message} is ${count}`;
+console.log(result);
