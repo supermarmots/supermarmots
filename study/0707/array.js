059d3a655062230d1016d59ab2bf6b7a1ba73fa8
ChanWoo Yun <yunchanwo@naver.com>
2025-07-07 15:00:22 +0900

    Array 메서드 실습

---
+++ study/0707/array.js
@@ -0,0 +1,48 @@
+// 고차 배열 메서드
+const nums = [1, 2, 3, 4, 5];
+
+// map
+const squares = nums.map((n) => n ** 2);
+
+for (const val of squares) {
+  console.log(`val => ${val}`);
+}
+
+for (let i = 0; i < nums.length; i++) {
+  console.log(`nums[${i}] => ${nums[i]}, squares[${i}] => ${squares[i]}`);
+}
+
+// filter
+const evens = nums.filter((n) => n % 2 === 0);
+for (const val of evens) {
+  console.log(`val =>${val}`);
+}
+
+// reduce
+const sum = nums.reduce((acc, cur) => {
+  acc += cur;
+  console.log(`acc => ${acc}, cur => ${cur}`);
+  return acc;
+}, 0);
+
+console.log(`sum => ${sum}`);
+
+// find
+const found = nums.find((n) => n > 2);
+console.log(`found => ${found}`);
+
+// some
+const hasNegative = nums.some((n) => n < 2);
+console.log(hasNegative);
+
+// every
+const allPositive = nums.every((n) => n > 1);
+console.log(allPositive);
+
+// flat
+const nested = [1, 2, 3];
+const duplicated = nested.flatMap((n) => [n, n]);
+for (let nest of duplicated) {
+  console.log(`nest => ${nest}`);
+}
+console.log(duplicated);
