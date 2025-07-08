16abeef68fea80caa90dd2ce715e41880872e775
ChanWoo Yun <yunchanwo@naver.com>
2025-07-08 14:34:43 +0900

    ch01_5 실습

--- study/Jul/0708/ch01_5/src/App.tsx
+++ study/Jul/0708/ch01_5/src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as D from "./data";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+      </p>
+      <img src={D.randomAvatar()} height="50" alt="호출 오류" />
+      <img src={D.randomImage()} height="300" alt="호출 오류" />
     </div>
   );
 }
