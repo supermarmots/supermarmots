6a4afa4e0583861f7e49d6f6ab4016ebbf5f7830
ChanWoo Yun <yunchanwo@naver.com>
2025-07-14 12:27:12 +0900

    Feat: JustifyCenterTest

--- study/Jul/0714/ch03_4/src/pages/JustifyCenterTest.tsx
+++ study/Jul/0714/ch03_4/src/pages/JustifyCenterTest.tsx
@@ -1,10 +1,26 @@
-import { Title } from "../components";
+import { Div, Title, Subtitle } from "../components";
+import * as D from "../data";
 
-export default function CopyMe() {
+export default function JustifyCenterTest() {
+  const boxes = D.range(0, 5).map((index) => (
+    <Div key={index} className="w-4 h-4 m-1 bg-black" />
+  ));
+
   return (
-    <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="m4-4"></div>
+    <section className="p-4 mt-4">
+      <Title>JustifyCenterTest</Title>
+      <div className="m4-4">
+        <Subtitle>flex flex-row justify-center</Subtitle>
+        <div className="flex flex-row justify-center h-40 bg-gray-300">
+          {boxes}
+        </div>
+      </div>
+      <div className="mt-4">
+        <Subtitle>flex flex-col justify-center</Subtitle>
+        <div className="flex flex-col justify-center h-40 bg-gray-300">
+          {boxes}
+        </div>
+      </div>
     </section>
   );
 }
