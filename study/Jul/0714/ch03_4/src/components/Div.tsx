1d89094bf898d494e2489acc70f9346d0f33120f
ChanWoo Yun <yunchanwo@naver.com>
2025-07-14 12:19:55 +0900

    Feat: WrapTest & MinMaxWidthHeight

--- study/Jul/0714/ch03_4/src/components/Div.tsx
+++ study/Jul/0714/ch03_4/src/components/Div.tsx
@@ -6,6 +6,7 @@ import type {
 } from "react";
 import type { WidthHeight } from "./WidthHeight";
 import type { LeftRightTopBottom } from "./LeftRightTopBottom";
+import type { MinMaxWidthHeight } from "./MinMaxWidthHeight";
 
 export type ReactDivProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
@@ -13,18 +14,22 @@ export type ReactDivProps = DetailedHTMLProps<
 >;
 export type DivProps = ReactDivProps &
   PropsWithChildren<WidthHeight> &
-  LeftRightTopBottom & {
+  LeftRightTopBottom &
+  MinMaxWidthHeight & {
     src?: string;
   };
 
 //prettier-ignore
 export const Div: FC<DivProps> = ({
   width, height, style: _style, src, className: _className,
-  left, right, top, bottom, ...props
+  left, right, top, bottom,
+  minWidth, maxWidth, minHeight, maxHeight
+  , ...props
 }) => {
   const style = {
     ..._style, width, height, backgroundImage: src && `url(${src})`,
-    left, right, top, bottom
+    left, right, top, bottom,
+    minWidth, maxWidth, minHeight, maxHeight
   }
   const className = ['box-border', src && 'bg-gray-300', _className].join(' ')
   return <div {...props} className={className} style={style} />
--- study/Jul/0714/ch03_4/src/pages/MinMaxTest.tsx
+++ study/Jul/0714/ch03_4/src/pages/MinMaxTest.tsx
@@ -1,10 +1,19 @@
-import { Title } from "../components";
+import { Div, Title } from "../components";
+import * as D from "../data";
 
-export default function CopyMe() {
+export default function MinMaxTest() {
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="m4-4"></div>
+      <Title>MinMaxTest</Title>
+      <Div className="p-4 bg-gray-300">
+        <Div src={D.randomImage(800, 300)} className="bg-cover">
+          <Div
+            className="w-1/2 bg-blue-500 h-80"
+            minWidth="300px"
+            maxWidth="500px"
+          />
+        </Div>
+      </Div>
     </section>
   );
 }
