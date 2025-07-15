a36a493eb618938120225ec70078cf2c505b0ef6
ChanWoo Yun <yunchanwo@naver.com>
2025-07-15 15:41:28 +0900

    Feat: useEffect, useState, 커스텀 훅훅 잽잽 원 투

--- study/Jul/0715/ch04_1/src/App.tsx
+++ study/Jul/0715/ch04_1/src/App.tsx
@@ -1,6 +1,23 @@
+import {useClock} from './hooks'
 import Clock from './pages/Clock'
-
+// 커스텀 훅
 export default function App() {
-  let today = new Date()
+  const today = useClock()
   return <Clock today={today} />
 }
+
+// useEffect, useState을 이용한 훅훅 잽잽 원 투
+// import Clock from './pages/Clock'
+// import {useEffect, useState} from 'react'
+
+// export default function App() {
+//   const [today, setToday] = useState(new Date())
+//   useEffect(() => {
+//     const duration = 1000
+//     const id = setInterval(() => {
+//       setToday(new Date())
+//     }, duration)
+//     return () => clearInterval(id)
+//   }, [])
+//   return <Clock today={today} />
+// }
