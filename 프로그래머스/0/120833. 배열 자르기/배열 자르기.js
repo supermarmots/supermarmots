0c32df2d8f71e555a7093c6ab635d587cc8eedef
whobuythis <163419609+whobuythis@users.noreply.github.com>
2025-07-15 10:17:26 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -0,0 +1,9 @@
+function solution(numbers, num1, num2) {
+    return numbers.slice(num1, num2 + 1)
+    // 테스트 1만 통과한 내 풀이
+    // var answer = [];
+    // for(let i=num1; numbers[i] <= num2; i ++) {
+    //     answer = numbers.slice(num1, num2 +1)
+    // }
+    // return answer;
+}
