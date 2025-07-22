4531006b97e8546ed56934b4eb36490bd51ba256
test <test@example.com>
2025-07-22 16:08:14 +0900

    Feat: ch05_2_카운터 만들기

--- study/Jul/0722/ch05_2/src/pages/CounterTest.tsx
+++ study/Jul/0722/ch05_2/src/pages/CounterTest.tsx
@@ -1,8 +1,31 @@
-export default function CopyMe() {
+import {useCallback} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {Title, Subtitle} from '../components'
+import {Icon} from '../theme/daisyui'
+import type {AppState} from '../store'
+import * as C from '../store/counter'
+
+export default function CounterTest() {
+  const dispatch = useDispatch()
+
+  const counter = useSelector<AppState, C.State>(({counter}) => counter)
+  const increase = useCallback(() => {
+    dispatch(C.increaseCounter())
+  }, [dispatch])
+  const decrease = useCallback(() => {
+    dispatch(C.decreaseCounter())
+  }, [dispatch])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>CounterTest</Title>
+      <div className="flex justify-center p-4 mt-4">
+        <div className="flex items-center justify-around w-1/3 text-blue-500 text-bold">
+          <Icon name="add_circle" iconClassName="text-3xl" onClick={increase} />
+          <Subtitle>{counter}</Subtitle>
+          <Icon name="remove_circle" iconClassName="text-3xl" onClick={decrease} />
+        </div>
+      </div>
     </section>
   )
 }
