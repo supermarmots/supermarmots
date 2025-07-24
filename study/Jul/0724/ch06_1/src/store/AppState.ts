c9e53f3c17e0feabc3cf93410e53b163abe567f8
test <test@example.com>
2025-07-24 16:17:12 +0900

    ch06_1 & ch06_2

---
+++ study/Jul/0724/ch06_1/src/store/AppState.ts
@@ -0,0 +1,12 @@
+// todo
+import * as L from './listEntities'
+import * as LO from './listidOrders'
+import * as LC from './listidCardidOrders'
+import * as C from './cardEntities'
+
+export type AppState = {
+  listEntities: L.State
+  listidOrders: LO.State
+  listidCardidOrders: LC.State
+  cardEntities: C.State
+}
