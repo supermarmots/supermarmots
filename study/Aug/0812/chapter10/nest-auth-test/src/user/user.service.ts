d8ab123a112254c64dad309b2e780195405aaf4f
whobuythis <yunchanwo@naver.com>
2025-08-12 16:11:45 +0900

    feat: Chapter10 finished

--- study/Aug/0812/chapter10/nest-auth-test/src/user/user.service.ts
+++ study/Aug/0812/chapter10/nest-auth-test/src/user/user.service.ts
@@ -8,4 +8,27 @@ export class UserService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>
   ) {}
+
+  createUser(user): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async getUser(email: string) {
+    const result = await this.userRepository.findOne({
+      where: { email },
+    });
+    return result;
+  }
+  async updateUser(email, _user) {
+    const user: User = await this.getUser(email);
+    console.log(_user);
+    user.username = _user.username;
+    user.password = _user.password;
+    console.log(user);
+    return this.userRepository.save(user);
+  }
+
+  deleteUser(email: string) {
+    return this.userRepository.delete({ email });
+  }
 }
