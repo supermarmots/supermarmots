59e8df633bdcefce1113819a5c3ca5d22223bf00
whobuythis <yunchanwo@naver.com>
2025-08-14 15:06:47 +0900

    Add: 주석 추가

--- study/Aug/0814/chapter13/nest-chat/src/app.gateway.ts
+++ study/Aug/0814/chapter13/nest-chat/src/app.gateway.ts
@@ -24,22 +24,24 @@ export class ChatGateway {
 
 @WebSocketGateway({ namespace: "room" })
 export class RoomGateway {
-  constructor(private readonly ChatGateway: ChatGateway) {}
+  constructor(private readonly ChatGateway: ChatGateway) {} // 채팅 게이트웨이 의존성 주입
   rooms = [];
 
-  @WebSocketServer()
+  @WebSocketServer() // 서버 인스턴스 접근을 위한 변수 선언
   server: Server;
 
   @SubscribeMessage("createRoom")
   handleMessage(@MessageBody() data) {
+    // 소켓 없이 데이터만 받음
     const { nickname, room } = data;
     this.ChatGateway.server.emit("notice", {
       message: `${nickname}님이 ${room}방을 만들었습니다. `,
     });
-    this.rooms.push(room);
-    this.server.emit("rooms", this.rooms);
+    this.rooms.push(room); // 채팅방 정보 받아서 추가
+    this.server.emit("rooms", this.rooms); // ROOMS 이벤트로 채팅방 리스트 전송
   }
 
+  // 방에 입장 시 실행되는 핸들러 메서드
   @SubscribeMessage("joinRoom")
   handleJoinRoom(socket: Socket, data) {
     const { nickname, room, toLeaveRoom } = data;
@@ -47,14 +49,16 @@ export class RoomGateway {
     this.ChatGateway.server.emit("notice", {
       message: `${nickname}님이 ${room}방에 입장했습니다. `,
     });
-    socket.join(room);
+    socket.join(room); // 새로운 방에 입장
   }
 
+  // RoomGateway로 message 이벤트가 오면 처리
   @SubscribeMessage("message")
   handleMessageToRoom(socket: Socket, data) {
     const { nickname, room, message } = data;
     console.log(data);
     socket.broadcast.to(room).emit("message", {
+      // 나 이외의 사람에게 데이터 전송
       message: `${nickname}: ${message}`,
     });
   }
--- study/Aug/0814/chapter13/nest-chat/static/script.js
+++ study/Aug/0814/chapter13/nest-chat/static/script.js
@@ -1,7 +1,7 @@
 const socket = io("http://localhost:3000/chat");
-const roomSocket = io("http://localhost:3000/room");
+const roomSocket = io("http://localhost:3000/room"); // 채팅방용 네임스페이스
 const nickname = prompt("닉네임을 입력해 주세요.");
-let currentRoom = "";
+let currentRoom = ""; // 채팅방 초깃값
 
 function sendMessage() {
   if (currentRoom === "") {
@@ -10,7 +10,7 @@ function sendMessage() {
   }
   const message = $("#message").val();
   const data = { message, nickname, room: currentRoom };
-  $("#chat").append(`<div>나 : ${message}</div>`);
+  $("#chat").append(`<div>나 : ${message}</div>`);  // 내가 보낸 메시지 바로 추가
   roomSocket.emit("message", data);
   return false;
 }
@@ -26,6 +26,7 @@ socket.on("message", (data) => {
   $("#chat").append(`<div>${data.message}</div>`);
 });
 
+// notice(공지) 이벤트를 받아서 처리
 socket.on("notice", (data) => {
   console.log(data);
   $("#notice").append(`<div>${data.message}</div>`);
@@ -36,16 +37,18 @@ roomSocket.on("message", (data) => {
   $("#chat").append(`<div>${data.message}</div>`);
 });
 
+// 클라이언트 측에서 채팅방 추가 함수
 roomSocket.on("rooms", (data) => {
   console.log(data);
-  $("#rooms").empty();
-  data.forEach((room) => {
+  $("#rooms").empty();  // 채빙방 갱신 시 일단 리스트를 비움
+  data.forEach((room) => {  // 루프를 돌면서 서버에서 준 데이터 추가
     $("#rooms").append(
       `<li>${room} <button onclick="joinRoom('${room}')">join</button></li>`
     );
   });
 });
 
+// 채팅방 생성 함수
 function createRoom() {
   const room = prompt("생성하실 방의 이름을 입력해주세요.");
   roomSocket.emit("createRoom", { room, nickname });
@@ -54,7 +57,8 @@ function createRoom() {
 
 // 방에 들어갈 때 기존에 있던 방에서는 나간다.
 function joinRoom(room) {
+  // 서버 측의 joinRoom 이벤트를 발생
   roomSocket.emit("joinRoom", { room, nickname, toLeaveRoom: currentRoom });
-  currentRoom = room;
+  currentRoom = room; // 현재 들어 있는 방의 값을 변경
   return false;
 }
