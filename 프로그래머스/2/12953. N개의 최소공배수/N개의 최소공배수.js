c991662aa3d079388b0993a88653f62fda5f899e
ChanWoo Yun <yunchanwo@naver.com>
2025-10-09 18:39:55 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,11 @@
+function getGcd(a, b) {
+    if (b === 0) {
+        return a;
+    } else {
+    return getGcd(b, a % b);
+    }
+}
+
+function solution(arr) {
+    return arr.reduce((a, b) => (a * b) / getGcd(a, b));
+}
